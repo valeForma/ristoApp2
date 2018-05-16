1d980526375a9cbd5763659abede1c5f67416b39
vale <vale@noconnection.local>
2018-05-16 17:33:01 +0200

    app presentation

--- src/app/shared/fab-button.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FabButtonDirective } from './sidenav.directive';
-
-describe('FabButtonDirective', () => {
-  it('should create an instance', () => {
-    const directive = new FabButtonDirective();
-    expect(directive).toBeTruthy();
-  });
-});
